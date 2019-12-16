97f7f653a4d0831b11db19c04b6ca53bccb68bcf
Jikme <Jikme@github.com>
2019-12-16 13:11:22 +0000

    Project Folders Cleanup

--- utilities.js
+++ utilities.js
@@ -2,6 +2,6 @@
 // 12/2019
 
 // JQUERY ref: https://getbootstrap.com/docs/4.4/components/scrollspy/
-// targets the ink that is clicked then sets it active
+// targets the link that is clicked then sets it active
 // for this implementation we have a one page app so scrolling will auto focus the nav to the correct link
 $("body").scrollspy({ target: "#navbar-top" });
