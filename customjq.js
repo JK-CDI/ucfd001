9005eaec533b3a626b83855bc895dce3e00eb340
Jikme <Jikme@github.com>
2019-12-17 13:36:50 +0000

    Boostrap JQ Helpers

---
+++ customjq.js
@@ -0,0 +1,4 @@
+// JQUERY ref: https://getbootstrap.com/docs/4.4/components/scrollspy/
+// targets the link that is clicked then sets it active
+// for this implementation we have a one page app so scrolling will auto focus the nav to the correct link
+$("body").scrollspy({ target: "#navbar-top" });
